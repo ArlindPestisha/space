d07dd13af637ad14e201e4b3c1212335ce8fc85b
ArlindPestisha <arlind.pestisha@hyperisland.se>
2024-05-10 13:40:53 +0200

    Add img and font

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
-
-function App() {
+const App = () => {
   
 
   return (
-    <div className="bg-black">
-      <h1 className="text-red-500 bg-black">Hello</h1>
-      <h2>Arlind Pestisha</h2>
+    <div className="bg-cover bg-center h-screen bg-home max-sm:bg-homeTablet" >
+      <h1 className="text-red-500  font-barlowCondensed">Hello</h1>
+      <h2 className=" font-bellefair">Arlind Pestisha</h2>
     </div>
   )
 }
