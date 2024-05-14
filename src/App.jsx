0dc9504e2fc7c3e11239a50716bcdc57e5812fa2
ArlindPestisha <arlind.pestisha@hyperisland.se>
2024-05-14 14:04:16 +0200

    Add Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Destination from './components/Destination';
-import Crew from './components/Crew';
-import Technology from './components/Technology';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Destination from './components/Destination'
+import Crew from './components/Crew'
+import Technology from './components/Technology'
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        
-          <Route path="/" element={<Home />} />
-          <Route path="/destination" element={<Destination />} />
-          <Route path="/crew" element={<Crew />} />
-          <Route path="/technology" element={<Technology />} />
-        
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/destination" element={<Destination />} />
+                <Route path="/crew" element={<Crew />} />
+                <Route path="/technology" element={<Technology />} />
+            </Routes>
+        </Router>
+    )
 }
 
-export default App;
+export default App
