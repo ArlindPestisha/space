bd514e236881e4a4a9490689f88bdc053ebbd490
ArlindPestisha <arlind.pestisha@hyperisland.se>
2024-05-17 13:21:47 +0200

    Fetch data from data.json

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,10 +1,62 @@
+import { useState, useEffect } from 'react'
 import Nav from './Nav'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Destination = () => {
+    const [destinations, setDestinations] = useState([])
+    // const [selectedDestination, setSelectedDestination] = useState(null)
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const selectedDestination =
+        location.state?.selectedDestination || destinations[0]
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        try {
+            const res = await fetch('/data.json')
+            const data = await res.json()
+            setDestinations(data.destinations)
+            console.log('Data', data)
+        } catch (error) {
+            console.error('Error fetching data', error)
+        }
+    }
+
+    const handleClick = (destination) => {
+        navigate('/destination', {
+            state: { selectedDestination: destination },
+        })
+    }
+
     return (
         <div className="h-screen bg-destinationDesktop bg-cover bg-center">
             <Nav />
-            <h1 className=" text-white">Hello</h1>
+            <ul className="text-white">
+                {destinations.map((destination) => (
+                    <li
+                        key={destination.name}
+                        onClick={() => handleClick(destination)}
+                    >
+                        {destination.name}
+                    </li>
+                ))}
+            </ul>
+            {selectedDestination && (
+                <div className="text-white">
+                    <img
+                        src={selectedDestination.images.webp}
+                        alt={selectedDestination.name}
+                    />
+                    <h2>{selectedDestination.name}</h2>
+                    <p>Description: {selectedDestination.description}</p>
+                    <p>Distance: {selectedDestination.distance}</p>
+                    <p>Travel Time: {selectedDestination.travel}</p>
+                </div>
+            )}
         </div>
     )
 }
